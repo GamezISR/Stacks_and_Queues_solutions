0de86a85c66842e46b0eaea5bd8166377c4a4af4
Gonzalo Gamez <88616733+GamezISR@users.noreply.github.com>
2021-11-16 08:22:25 +0000

    Comments for portions

--- main.py
+++ main.py
@@ -27,25 +27,34 @@ def isEmpty(stackName):
 print("First stack: " + str(first_Stack))
 print("Second stack: " + str(second_Stack))
 print("Third stack: " + str(third_Stack))
+
 # push a 3 onto first stack than print stack
+"""
 push(3, first_Stack)
 print("Pushed First stack: " + str(first_Stack))
+"""
 
 # push a 7 and 2 into the third stack and print
+"""
 push(7, third_Stack)
 push(2, third_Stack)
 print("Pushed third stack: " + str(third_Stack))
+"""
 
 # pop all elements in second stack in push them into the third stack than print
+"""
 while isEmpty(second_Stack) == False:
   push(pop(second_Stack),third_Stack)
 print("Popped second stack: " + str(second_Stack))
 print("Pushed third stack: " + str(third_Stack))
+"""
 
 # check all stacks with isEmpty
+"""
 print(isEmpty(first_Stack))
 print(isEmpty(second_Stack))
 print(isEmpty(third_Stack))
+"""
 
 
 
@@ -77,31 +86,39 @@ def isEmpty(queueName):
       return True
     else:
       return False
-
+"""
 print("First queue: " + str(first_Queue))
 print("Second queue: " + str(second_Queue))
 print("Third queue: " + str(third_Queue))
+"""
 
 # enqueue a 3 onto first queue than print 
+"""
 enqueue(3, first_Queue)
 print("Enqueued First Queue: " + str(first_Queue))
+"""
 
 # enqueue a 7 and 2 into the third queue and print
+"""
 enqueue(7, third_Queue)
 enqueue(2, third_Queue)
 print("Enqued third Queue: " + str(third_Queue))
+"""
 
 # dequeue all elements in second queue and enqueue into the third queue than print
+"""
 while isEmpty(second_Queue) == False:
   enqueue(dequeue(second_Queue), third_Queue)
 
 print("Dequeued second queue: " + str(second_Queue))
 print("Enqued third queue: " + str(third_Queue))
+"""
 
 # print all queues with isEmpty
+"""
 print(isEmpty(first_Queue))
 print(isEmpty(second_Queue))
 print(isEmpty(third_Queue))
-
+"""
 
 
